73769901079cea59a775ef173494f66c50529ac8
CampingTurret <lorenzo.gaeta@hotmail.nl>
2025-03-04 12:14:57 +0100

    made loader general using config file, for linux use / instead of \

--- Run_Algortims.py
+++ Run_Algortims.py
@@ -6,12 +6,24 @@ import pathlib
 import numpy as np
 import time
 
+
 if __name__ == "__main__":
+    f = open('Confix.txt')
+    for line in f.readlines():
+        p, v = line.replace('\n','').split(':')
+        if p == 'f1':
+            f1 = v
+        if p == 'f2':
+            f2 = v
+        if p == 'seperator':
+            seperator = v
+    print(f'Images{seperator}{f1}')
+    f.close()
     Images = Get_Image_dict()
-    f1 = "cyberzoo_poles_panels_mats" #cyberzoo_aggressive_flight
-    f2 = "20190121-142935"#20190121-144646
+    #f1 = "cyberzoo_poles_panels_mats" #cyberzoo_aggressive_flight
+    #f2 = "20190121-142935"#20190121-144646
     res = {}
-    ex_images: dict = Images[f"Images\\{f1}"][f"Images\\{f1}\\{f2}"]
+    ex_images: dict = Images[f"Images{seperator}{f1}"][f"Images{seperator}{f1}{seperator}{f2}"]
     I_len = len(ex_images)
     for sub in Base_Algorthm.__subclasses__():
         dt = time.time()
